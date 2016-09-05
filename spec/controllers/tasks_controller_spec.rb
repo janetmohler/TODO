a0c2658afaa41c7b67b8ce09b009a3f80777dca0
Janet Mohler <janetmohler82@gmail.com>
2016-09-05 06:08:51 +0000

    Added ordering to tasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
   describe "tasks#index" do
-    it "should list the tasks in the database" do   
-    task1 = FactoryGirl.create(:task)
-    task2 = FactoryGirl.create(:task)
-    get :index
-    expect(response).to have_http_status :success
-    response_value = ActiveSupport::JSON.decode(@response.body)
-    expect(response_value.count).to eq(2)
+    it "should list the tasks in the database" do
+      task1 = FactoryGirl.create(:task)
+      task2 = FactoryGirl.create(:task)
+      task1.update_attributes(title: "Something else")
+      get :index
+      expect(response).to have_http_status :success
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value.count).to eq(2)
+      response_ids = response_value.collect do |task|
+        task["id"]
+      end
+      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
 
@@ -21,5 +26,5 @@ RSpec.describe TasksController, type: :controller do
       expect(task.done).to eq(true)
     end
   end
-  
+
 end
